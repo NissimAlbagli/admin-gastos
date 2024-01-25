d974cc3be02c0fe689fa754ca3f1d685c62cb69b
Nissim Albagli <nissimalbagli@gmail.com>
2024-01-25 16:27:01 -0300

    Funcionalidad de mostrar el porcentaje en grafico y de forma numerica

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -1,4 +1,5 @@
 <script setup>
+    import { computed } from 'vue';
     import CircleProgress from 'vue3-circle-progress';
     import "vue3-circle-progress/dist/circle-progress.css";
     import { formatearCantidad } from '../helpers';
@@ -19,19 +20,25 @@
             required: true
         }
     })
+
+    const porcentaje = computed(() => {
+       return parseInt(((props.presupuesto - props.disponible) / props.presupuesto) * 100)
+    })
 </script>
 
 <template>
     <div class="dos-columnas">
         <div class="contenedor-grafico">
             
+            <p class="porcentaje">{{ porcentaje }}%</p>
+
             <CircleProgress 
-            :percent="50"
-            :size="250"
-            :border-width="25"
-            :border-bg-width="25"
-            fill-color="#3b82f6"
-            empty-color="#e1e1e1"
+                :percent="porcentaje"
+                :size="250"
+                :border-width="30"
+                :border-bg-width="30"
+                fill-color="#3b82f6"
+                empty-color="#e1e1e1"
             />
         </div>
 
@@ -62,6 +69,23 @@
 
 
 <style scoped>
+    .contenedor-grafico {
+        position: relative;
+    }
+
+    .porcentaje {
+        position: absolute;
+        margin: auto;
+        top: calc(50% - 1.5rem);
+        left: 0;
+        right: 0;
+        text-align: center;
+        z-index: 1;
+        font-size: 3rem;
+        font-weight: 900;
+        color: var(--gris-oscuro);
+    }
+
     .dos-columnas {
         display: flex;
         flex-direction: column;
